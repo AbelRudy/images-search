00f7947cd95eefcb5cd558d919972c70a9f9a5a5
AbelRudy <nabelrudy@yahoo.fr>
2022-10-10 19:18:54 +0200

    All selection functions

--- src/utils/selectionFunctions.js
+++ src/utils/selectionFunctions.js
@@ -4,8 +4,33 @@ export async function selectAllContexts() {
 	return await parseXML("contexte").then((data) => data.contextes.Contexte);
 }
 
-export async function selectContextByName(conceptName) {
+export async function selectContextByName(contextName) {
 	return await parseXML("contexte")
 		.then((data) => data.contextes.Contexte)
-		.then((data) => data.filter((d) => d.Name === conceptName));
+		.then((data) => data.filter((d) => d.Name === contextName))
+		.then((data) => data[0]);
+}
+
+export async function selectConceptByName(conceptName) {
+	return await parseXML("concept")
+		.then((data) => data.concepts.concept)
+		.then((data) => data.filter((d) => d.Name === conceptName))
+		.then((data) => data[0]);
+}
+
+export async function selectAllConceptsFromContext(contextName) {
+	let concepts = [];
+	return await selectContextByName(contextName)
+		.then((data) => data.concept)
+		.then((data) => {
+			if (Array.isArray(data))
+				return data.map((d) =>
+					selectConceptByName(d.ConceptName).then((data) => concepts.push(data))
+				);
+			else
+				return selectConceptByName(data.ConceptName).then((data) =>
+					concepts.push(data)
+				);
+		})
+		.then((data) => concepts);
 }
