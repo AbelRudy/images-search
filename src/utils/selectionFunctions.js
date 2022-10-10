5008cfd67f6dea9abd612a447304fc4e072536e0
AbelRudy <nabelrudy@yahoo.fr>
2022-10-10 14:43:53 +0200

    SelectContextByName function

--- src/utils/selectionFunctions.js
+++ src/utils/selectionFunctions.js
@@ -1,5 +1,11 @@
 import parseXML from "./ParseXML";
 
 export async function selectAllContexts() {
-	return await parseXML("contexte").then(data => data.contextes.Contexte);
+	return await parseXML("contexte").then((data) => data.contextes.Contexte);
+}
+
+export async function selectContextByName(conceptName) {
+	return await parseXML("contexte")
+		.then((data) => data.contextes.Contexte)
+		.then((data) => data.filter((d) => d.Name === conceptName));
 }
