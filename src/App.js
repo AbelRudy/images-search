60eaf5556d02f477d51c9f6b73489bd8bb3aad7f
Maxime-ng <ngouamem@3il.fr>
2022-10-19 10:15:01 +0200

    tagcloud

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React from 'react';
+
+import { TagCloud } from 'react-tagcloud';
+
+const data = [
+    { value: 'Bastios Beez', count: 38 },
+    { value: 'Ghost', count: 30 },
+    { value: 'Caramel', count: 28 },
+    { value: 'Gremier 95', count: 25 },
+    { value: 'Sophia', count: 33 },
+    { value: 'Amine', count: 18 },
+    { value: 'Eliza', count: 20 },
+  ]
+
+  const SimpleCloud = ()=>(
+    <TagCloud
+        minSize={12}
+        maxSize={35}
+        tags={data}
+        onClick={tag => alert(`'${tag.value}' sélectionné!`)}
+        />
+  )
+
 function App() {
 	return (
-		<div>
-			<p>Hello world</p>
-		</div>
+		SimpleCloud.apply()
 	);
 }
 
